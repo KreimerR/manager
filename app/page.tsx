b01b92e1098d1c65210193979944906083458c80
Rodion Kreimer <kreimerrodion@gmail.com>
2026-03-07 12:40:42 +0200

    feat: add Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-export default function Home() {
-  return (
-    <div></div>
-  )
+import { redirect } from "next/navigation"
+import { auth } from "@/lib/auth"
+
+export default async function Home() {
+  const session = await auth()
+
+  if (!session?.user) redirect("/authorization")
+
+  redirect(`/boards`)
 }
