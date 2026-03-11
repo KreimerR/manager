43a78ae98dfef8f5dace74799506f09bf77a7935
Rodion Kreimer <kreimerrodion@gmail.com>
2026-03-11 22:00:43 +0200

    update: update the authorization page

--- app/authorization/page.tsx
+++ app/authorization/page.tsx
@@ -9,9 +9,9 @@ export default async function Authorization() {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center gap-5 w-full">
-        <h1 className="text-4xl font-bold">Welcome to Manager</h1>
-        <p className="text-gray-700 text-sm">Manage all your projects and personal duties in one application</p>
+      <div className="flex flex-col items-center gap-5 w-full px-20">
+        <h1 className="text-3xl font-bold">Welcome to Manager</h1>
+        <p className="text-gray-700 text-sm text-center">Manage all your projects and personal duties in one application</p>
         <LogIn />
       </div>
     </div>
--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react"
 
 export default function LogIn() {
   return (
-    <div className="flex flex-col items-center gap-5 w-[400px]">
+    <div className="flex flex-col items-center gap-5 w-full">
       <button
         className="p-2 rounded-lg w-full text-gray-700 outline-1 outline-gray-700 transition-all hover:cursor-pointer hover:bg-gray-100"
         onClick={() => signIn("google")}
