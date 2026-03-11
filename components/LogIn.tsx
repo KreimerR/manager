a5c27b677097871c65c47b3f09fe1e1ecc275984
Rodion Kreimer <kreimerrodion@gmail.com>
2026-03-11 22:06:08 +0200

    update: update the authorization page

--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react"
 
 export default function LogIn() {
   return (
-    <div className="flex flex-col items-center gap-5 w-full">
+    <div className="flex flex-col items-center gap-5 w-[70vw] lg:w-[400px]">
       <button
         className="p-2 rounded-lg w-full text-gray-700 outline-1 outline-gray-700 transition-all hover:cursor-pointer hover:bg-gray-100"
         onClick={() => signIn("google")}
