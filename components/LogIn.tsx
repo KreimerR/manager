9e68b36437297c942da3cb571124ad45e10257eb
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 21:07:38 +0300

    style: update styling on the LogIn component

--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -6,7 +6,7 @@ export default function LogIn() {
   return (
     <div className="flex flex-col items-center gap-5 w-[70vw] lg:w-[400px]">
       <button
-        className="p-2 rounded-lg w-full text-gray-700 outline-1 outline-gray-700 transition-all hover:cursor-pointer hover:bg-gray-100"
+        className="p-2 rounded-lg max-w-[400px] lg:max-w-none w-full text-gray-700 outline-1 outline-gray-700 transition-all hover:cursor-pointer hover:bg-gray-100"
         onClick={() => signIn("google")}
       >
         Sign in with Google
