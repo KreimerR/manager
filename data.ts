5f8b494c4c04c0294687ce440aabe734b8ce522b
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 20:57:16 +0300

    feat: add data

---
+++ data.ts
@@ -0,0 +1,25 @@
+import background1 from "@/public/background-1.jpg"
+import background2 from "@/public/background-2.jpg"
+import background3 from "@/public/background-3.jpg"
+import background4 from "@/public/background-4.jpg"
+
+const backgrounds = [
+  {
+    url: "https://images.pexels.com/photos/714258/pexels-photo-714258.jpeg",
+    background: background1,
+  },
+  {
+    url: "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg",
+    background: background2,
+  },
+  {
+    url: "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg",
+    background: background3,
+  },
+  {
+    url: "https://images.pexels.com/photos/1169754/pexels-photo-1169754.jpeg",
+    background: background4,
+  },
+]
+
+export default backgrounds
