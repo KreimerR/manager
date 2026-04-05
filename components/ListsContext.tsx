5ac1a11e8f88808268baa93bdbc0317e8b7cc16d
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 21:06:58 +0300

    feat: add the ListsContext component

---
+++ components/ListsContext.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { createContext, useContext, useState, useCallback, ReactNode } from "react"
+// import { useRouter } from "next/navigation";
+// import addNewList from "@/actions/addNewList"
+import { ListType, TaskType } from "@/types";
+import useLists from "@/hooks/useLists";
+
+const ListsContext = createContext<any | null>(null)
+
+export function ListsProvider({
+  lists,
+  tasks,
+  boardId,
+  children
+}: {
+  lists: ListType[],
+  tasks: TaskType[],
+  boardId: string,
+  children: ReactNode
+}) {
+  const listsLogic = useLists()
+
+  return (
+    <ListsContext.Provider value={{ ...listsLogic, lists, tasks, boardId }}>
+      {children}
+    </ListsContext.Provider>
+  );
+}
+
+export function useListsContext() {
+  const context = useContext(ListsContext)
+
+  if (!context) throw new Error("useListsContext must be used within ListsProvider")
+
+  return context
+};
