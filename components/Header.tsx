b5bae8e30c4216e5a96b82fa606b3bfc415ff58a
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 21:03:43 +0300

    feat: add logo to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,8 @@ import CreateButton from "./CreateButton"
 import { redirect } from "next/navigation"
 import { auth } from "@/lib/auth"
 import Link from "next/link"
+import Image from "next/image"
+import logo from "@/app/favicon.ico"
 import Profile from "./Profile"
 
 export default async function Header() {
@@ -11,7 +13,17 @@ export default async function Header() {
 
   return (
     <div className="flex justify-between align-center p-3 w-full bg-white">
-      <Link href="/" className="font-lg font-[600]">Manager</Link>
+      <Link href="/" className="flex items-center gap-2">
+        <Image
+          src={logo}
+          alt="Logo"
+          width={200}
+          height={200}
+          className="w-[40px] h-[40px]"
+        />
+
+        <span className="text-lg text-gray-700 font-bold">Manager</span>
+      </Link>
 
       <div className="flex items-center gap-5">
         <CreateButton />
