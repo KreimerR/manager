6776835fa1498360c6f1bd241686e11a55796df7
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 21:09:28 +0300

    feat: add the TaskEditingCompletion component

---
+++ components/TaskEditingCompletion.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useTaskContext } from "./TaskContext"
+
+export default function TaskEditingCompletion() {
+  const taskLogic = useTaskContext()
+
+  return (
+    taskLogic.task.completed ? (
+      <button
+        className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-blue-500 hover:text-white"
+        onClick={() => taskLogic.markTheTaskAsNotCompleted(taskLogic.task)}
+      >
+        Undone
+      </button>
+    ) : (
+      <button
+        className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-blue-500 hover:text-white"
+        onClick={() => taskLogic.markTheTaskAsCompleted(taskLogic.task)}
+      >
+        Done
+      </button>
+    )
+  )
+}
