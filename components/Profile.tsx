823e9db2124f09d0916799fbe1ebc41fdae3f993
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 21:08:05 +0300

    feat: update the Profile component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,11 +6,7 @@ import { signOut } from "next-auth/react"
 import deleteAccount from "@/actions/deleteAccount"
 import { useRouter } from "next/navigation"
 
-type Props = {
-  session: any
-}
-
-export default function Profile({ session }: Props) {
+export default function Profile({ session }: { session: any }) {
   const [profileOpened, setProfileOpened] = useState<boolean>(false)
 
   const router = useRouter()
@@ -24,37 +20,35 @@ export default function Profile({ session }: Props) {
   }
 
   return (
-    <div>
-      <div
-        className="relative"
-        onClick={() => setProfileOpened((prev: boolean) => !prev)}
-      >
-        <Image
-          src={session.user.image}
-          width={30}
-          height={30}
-          alt="User Icon"
-          className="object-fit rounded-full w-max h-max hover:cursor-pointer"
-        />
+    <div
+      className="relative"
+      onClick={() => setProfileOpened((prev: boolean) => !prev)}
+    >
+      <Image
+        src={session.user.image}
+        width={30}
+        height={30}
+        alt="User Icon"
+        className="object-fit rounded-full w-max h-max hover:cursor-pointer"
+      />
 
-        {profileOpened && (
-          <div className="flex flex-col absolute bottom-[-300%] right-0 w-[200px] bg-white rounded-2xl shadow-2xl z-30">
-            <button
-              className="w-full p-2 bg-white hover:cursor-pointer transition-colors hover:bg-gray-200 rounded-t-2xl"
-              onClick={() => signOut()}
-            >
-              Log out
-            </button>
+      {profileOpened && (
+        <div className="flex flex-col absolute bottom-[-300%] right-0 w-[200px] bg-white rounded-2xl z-30 shadow-lg outline-1 outline-gray-200">
+          <button
+            className="w-full p-2 text-gray-700 bg-white hover:cursor-pointer transition-colors hover:bg-gray-200 rounded-t-2xl"
+            onClick={() => signOut()}
+          >
+            Log out
+          </button>
 
-            <button
-              className="w-full p-2 bg-white hover:cursor-pointer transition-colors hover:bg-red-500 hover:text-white rounded-b-2xl"
-              onClick={deleteTheAccount}
-            >
-              Delete account
-            </button>
-          </div>
-        )}
-      </div>
+          <button
+            className="w-full p-2 text-gray-700 bg-white hover:cursor-pointer transition-colors hover:bg-red-500 hover:text-white rounded-b-2xl"
+            onClick={deleteTheAccount}
+          >
+            Delete account
+          </button>
+        </div>
+      )}
     </div>
   )
 }
