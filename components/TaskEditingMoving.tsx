0ac01cf7b24aa2d173c374f34028803f68a3328a
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-05 21:09:40 +0300

    feat: add the TaskEditingMoving component

---
+++ components/TaskEditingMoving.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useListContext } from "./ListContext"
+import { useListsContext } from "./ListsContext"
+import { useTaskContext } from "./TaskContext"
+
+export default function TaskEditingMoving() {
+  const taskLogic = useTaskContext()
+  const listLogic = useListContext()
+  const listsLogic = useListsContext()
+
+  return (
+    listLogic.taskMoving ? (
+      <>
+        <button
+          className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-red-500 hover:text-white"
+          onClick={() => taskLogic.deleteTheTask(taskLogic.task)}
+        >
+          Delete
+        </button>
+
+        <button
+          className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-gray-200"
+          onClick={() => taskLogic.stopSettingTheChosenTask(listsLogic.setTaskMoving, listsLogic.setChosenTask)}
+        >
+          Stop
+        </button>
+
+        <button
+          className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-gray-200"
+          onClick={() => taskLogic.stopEditing(listsLogic.setTaskMoving, listsLogic.setChosenTask)}
+        >
+          Close
+        </button>
+      </>
+    ) : (
+      <>
+        <button
+          className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-red-500 hover:text-white"
+          onClick={() => taskLogic.deleteTheTask(taskLogic.task)}
+        >
+          Delete
+        </button>
+
+        <button
+          className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-gray-200"
+          onClick={() => taskLogic.setTheChosenTask(taskLogic.task, listsLogic.setTaskMoving, listsLogic.setChosenTask)}
+        >
+          Move
+        </button>
+
+        <button
+          className="py-1 px-1 text-sm bg-gray-100 rounded-lg hover:cursor-pointer transition-colors hover:bg-gray-200"
+          onClick={() => taskLogic.setTaskEditing(false)}
+        >
+          Close
+        </button>
+      </>
+    )
+  )
+}
